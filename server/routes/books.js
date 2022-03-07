322b1b8a416f1a24db4c7fd68dcf766e7ceaa3ac
Vishal <vishalparekh130@gmail.com>
2022-03-06 22:01:03 -0500

    - Add if else block for add book and edit book in details.ejs

--- server/routes/books.js
+++ server/routes/books.js
@@ -14,6 +14,7 @@ router.get('/', (req, res, next) => {
       return console.error(err);
     }
     else {
+      console.log('Books',books)
       res.render('books/index', {
         title: 'Books',
         books: books
